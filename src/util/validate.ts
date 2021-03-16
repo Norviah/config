8f0e50d4648627a908de2275e0e1d798137f7cdf
Norviah <joseph.masresha.481@my.csun.edu>
2021-03-16 13:15:13 -0700

    structure: rename types.ts -> validate.ts

--- src/util/validate.ts
+++ src/util/validate.ts
@@ -1,11 +1,12 @@
 import { Types } from '../types/types';
+import { Strings } from '../types/typescript/strings';
 
 /**
  * As this project uses a config object to set values, and TypeScript doesn't
- * determine typings at run time, we'll be using this set to determine if a
+ * determine typings at run time, we'll be using an object to determine if a
  * given element is a certain type.
  */
-const types: { [key in Types]: (element: any) => boolean } = {
+const checks: { [key in Strings<Types>]: (element: any) => boolean } = {
   /**
    * type: string
    * Determines if the given element is a string.
@@ -31,41 +32,41 @@ const types: { [key in Types]: (element: any) => boolean } = {
   undefined: (element: any): boolean => typeof element === 'undefined',
 
   /**
-   * type: array<number>
+   * type: number[]
    * Determines if the given element is an array containing only numbers.
    */
-  'array<number>': (element: any): boolean => {
+  'number[]': (element: any): boolean => {
     return Array.isArray(element) && element.every((entry: any): boolean => typeof entry === 'number');
   },
 
   /**
-   * type: array<string>
+   * type: string[]
    * Determines if the given element is an array containing only strings.
    */
-  'array<string>': (element: any): boolean => {
+  'string[]': (element: any): boolean => {
     return Array.isArray(element) && element.every((entry: any): boolean => typeof entry === 'string');
   },
 
   /**
-   * type: array<boolean>
+   * type: boolean[]
    * Determines if the given element is an array containing only booleans.
    */
-  'array<boolean>': (element: any): boolean => {
+  'boolean[]': (element: any): boolean => {
     return Array.isArray(element) && element.every((entry: any): boolean => typeof entry === 'boolean');
   },
 };
 
 /**
- * Determines if the given element is any of the desired types.
+ * Determines if the given element is the desired type.
  * @param  element The element to check.
- * @param  list    The list of types to ensure the element complies with.
- * @return         Determines if the element's type exists within the list.
+ * @param  type    The type(s) to check for the given element.
+ * @return         Represents if the given element is the desired type.
  */
-export function check(element: any, type: Types | Types[]): boolean {
-  // As multiple desired types can be given within an array, we'll check if a
-  // single type is  given and cast it into an array so we can deal a single
-  // solution to check the type if either a string or an array is given.
+export function validate(element: any, type: Strings<Types> | Strings<Types>[]): boolean {
+  // As multiple types can be given, if a single type is given, we'll cast it
+  // into an array to have one solution for both possibilities.
   type = Array.isArray(type) ? type : [type];
 
-  return type.map((type: Types) => types[type](element)).some((entry: boolean) => entry === true);
+  // Next, we'll determine if the given element is one of the desired type.
+  return type.some((type: Strings<Types>): boolean => checks[type](element) === true);
 }
