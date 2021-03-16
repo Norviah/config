fcfe1c5811544fa8140b2082306f675c0a709b39
Norviah <21983700+Norviah@users.noreply.github.com>
2021-03-16 13:12:04 -0700

    structure: favor using record<string, any> over a custom object type

--- src/types/options.ts
+++ src/types/options.ts
@@ -1,12 +1,13 @@
-export interface Options<T> {
+export interface Options<T extends Record<string, any>> {
   /**
    * Represents the config's absolute path.
    */
   path?: string;
 
   /**
-   * If a user doesn't have a config set, this value could represent the default
-   * object for the config file, which will be saved to the config's path.
+   * Represents default values for the config. If a config file doesn't exist,
+   * this default object will be saved to the config's path. If this property
+   * isn't given, the typings object will be saved instead.
    */
   default?: T;
 }
