a86be3c7b5af8a3957ba7841e5a72de4eac2ef68
Norviah <joseph.masresha.481@my.csun.edu>
2021-03-16 13:13:35 -0700

    chore: update comments

--- src/util/paths.ts
+++ src/util/paths.ts
@@ -2,16 +2,16 @@ import { path } from 'app-root-path';
 import { join } from 'path';
 
 /**
- * Represents the absolute path for this project's root directory.
+ * The absolute path for the project's root.
  */
 export const root: string = path;
 
 /**
- * Represents the absolute path for the config directory.
+ * The absolute path for the config directory.
  */
 export const configDir: string = join(root, 'config');
 
 /**
- * Represents the absolute path for the config file.
+ * The absolute path for the config file.
  */
 export const config: string = join(configDir, 'config.json');
