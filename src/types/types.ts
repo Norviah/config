37a46e13ab4759dc8ace8ee6a3f1b382a98c395c
Norviah <joseph.masresha.481@my.csun.edu>
2021-07-30 21:57:20 -0700

    revert: remove the possiblity to have infinite objects as types

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,15 +3,4 @@ import { Strings } from './typescript/strings';
 /**
  * Represents the possible types that is supported within a config file.
  */
-export type Types =
-  | 'string'
-  | 'number'
-  | 'boolean'
-  | 'undefined'
-  | 'number[]'
-  | 'string[]'
-  | 'boolean[]'
-  | 'object<string>'
-  | 'object<number>'
-  | 'object<boolean>'
-  | { [key: string]: Types | Strings<Types>[] };
+export type Types = 'string' | 'number' | 'boolean' | 'undefined' | 'number[]' | 'string[]' | 'boolean[]' | { [key: string]: Types | Strings<Types>[] };
--- src/types/typings.ts
+++ src/types/typings.ts
@@ -2,12 +2,11 @@ import { Types } from './types';
 import { Strings } from './typescript/strings';
 
 type Values = Strings<Types> | Strings<Types>[];
-type Objects = 'undefined' | 'object<string>' | 'object<number>' | 'object<boolean>' | ('undefined' | 'object<string>' | 'object<number>' | 'object<boolean>')[];
 
 /**
  * As TypeScript only exists within compile time, we'll need a type to reference
  * desired types of a given interface within runtime.
  */
 export type Typings<T extends Record<string, any>> = {
-  [K in keyof Required<T>]: T[K] extends Record<string, unknown> ? Typings<T[K]> | Objects : Values;
+  [K in keyof Required<T>]: T[K] extends Record<string, unknown> ? Typings<T[K]> : Values;
 };
